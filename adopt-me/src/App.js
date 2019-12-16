73836cc92bbe66db846b72e4eac0fbc52a278fab
maxlvl <maxim@maxim2019.2ndsiteinc.com>
2019-12-16 13:39:30 -0500

    Tools setup

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,25 +1,30 @@
-const Pet = ({name, animal, breed}) => {
-	return React.createElement("div", {} ,[
-		React.createElement("h1", {}, name),
-		React.createElement("h2", {}, animal),
-		React.createElement("h2", {}, breed),
-	]);
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
 };
 
 const App = () => {
-	return React.createElement(
-		"div",
-		{},
-		[
-			React.createElement("h1", {}, "Adopt Me!"),
-			React.createElement(Pet, {"name": "Luna", "animal": "Dog", "breed": "German Shorthaired Pointer"}),
-			React.createElement(Pet, {"name": "Pumba", "animal": "Dog", "breed": "Poodle"}),
-			React.createElement(Pet, {"name": "Missy", "animal": "Cat", "breed": "A dumb one"})
-		]
-	);
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Oreo",
+      animal: "Dog",
+      breed: "German Shorthaired Pointer"
+    }),
+    React.createElement(Pet, {
+      name: "Pumba",
+      animal: "Dog",
+      breed: "Poodle"
+    }),
+    React.createElement(Pet, {
+      name: "Missy",
+      animal: "Cat",
+      breed: "A dumb one"
+    })
+  ]);
 };
 
-ReactDOM.render(
-	React.createElement(App),
-	document.getElementById("root")
-)
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
